1c9f41677e0e12a73507c0ef943228d0c6446df6
Martin Schijf <mschijf@gmail.com>
2025-02-04 22:38:02 +0100

    start.sh generieker gemaakt

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
+script_path=$(dirname $(realpath -s $0))
+
 program="home-monitor"
 path="home-monitor"
 port="8080"
 
+bash $script_path/stop.sh
+
 cd ~/$path
-bash ./script/stop.sh
 program_version=`ls -1 $program*.jar | tail -1`
 java -Dspring.profiles.active=prod -Dserver.port=$port -jar $program_version >> ./log/$program-log 2>> ./log/$program-log &
