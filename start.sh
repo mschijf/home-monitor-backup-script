7ce9c47a0811fbc7cb6f335a4b08d85b80f7e9a4
Martin Schijf <mschijf@gmail.com>
2025-02-05 18:17:54 +0100

    do update

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ port="8080"
 bash $script_path/stop.sh
 
 cd ~/$path
-program_version=`ls -1 $program*.jar | tail -1`
+program_version=`ls -1tr $program*.jar | tail -1`
 java -Dspring.profiles.active=prod -Dserver.port=$port -jar $program_version >> ./log/$program-log 2>> ./log/$program-log &
