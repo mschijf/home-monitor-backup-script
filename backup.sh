32a909041817af9704e0a7628b6408d2e221fd8f
Martin Schijf <mschijf@gmail.com>
2025-02-05 18:05:47 +0100

    updated and renamed crontab_content

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 backuptime=$(date +%Y%m%d_%H%M%S)
 
-cd ~/home-monitor/backup
+mkdir ~/tmp_backup
+cd ~/tmp_backup
+
 docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
 tar -czf backup_postgres.tar.gz ./backup_postgres.sql
 rm backup_postgres.sql
 
 echo -n $(date '+%Y-%m-%d %H:%M:%S')  
 echo -n '  '
-du -h0 ../backup/backup_postgres.tar.gz
+du -h0 backup_postgres.tar.gz
 echo -n '  '
 ~/dropbox-uploader/dropbox_uploader.sh upload ~/home-monitor/backup/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
-~/home-monitor/script/cleanup_backup.sh _postgres 672
+~/home-monitor-script/cleanup_backup.sh _postgres 672
 
+rm backup_postgres.tar.gz
+rmdir ~/tmp_backup
