d11a323bca5cf9ca6d11314649a51c0fadbb12a9
Martin Schijf <mschijf@gmail.com>
2025-02-06 20:35:12 +0100

    cleanup

--- backup.sh
+++ backup.sh
@@ -5,14 +5,14 @@ cd ~/tmp_backup
 
 docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
 tar -czf backup_postgres.tar.gz ./backup_postgres.sql
-rm backup_postgres.sql
 
 echo -n $(date '+%Y-%m-%d %H:%M:%S')  
 echo -n '  '
 du -h0 backup_postgres.tar.gz
 echo -n '  '
-~/dropbox-uploader/dropbox_uploader.sh upload ~/home-monitor/backup/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
+~/dropbox-uploader/dropbox_uploader.sh upload ~/tmp_backup/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
 ~/home-monitor-script/cleanup_backup.sh _postgres 672
 
 rm backup_postgres.tar.gz
+rm backup_postgres.sql
 rmdir ~/tmp_backup
--- stop.sh
+++
@@ -1,4 +0,0 @@
-program=home-monitor
-
-kill -9 `ps x | grep java | grep $program | awk '{print $1}'` 2> /dev/null
-
