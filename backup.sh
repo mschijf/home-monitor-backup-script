e937bdc030d7e72dc0702fc619c553d851bb5732
Martin Schijf <mschijf@gmail.com>
2025-02-08 20:33:39 +0100

    fix bug

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 backuptime=$(date +%Y%m%d_%H%M%S)
 
 backupdir=/tmp/home-monitor-backup 
-mkdir -D $backupdir
+mkdir $backupdir
 cd $backupdir
 
 docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
@@ -11,7 +11,7 @@ echo -n $(date '+%Y-%m-%d %H:%M:%S')
 echo -n '  '
 du -h0 backup_postgres.tar.gz
 echo -n '  '
-~/dropbox-uploader/dropbox_uploader.sh upload ~/tmp_backup/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
+~/dropbox-uploader/dropbox_uploader.sh upload $backupdir/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
 
 script_path=$(dirname $(realpath -s $0))
 $script_path/cleanup_backup.sh _postgres 672
