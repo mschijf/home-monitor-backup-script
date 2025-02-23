4f38dd21f56f3d556c9b8a2685ee116b7df18752
Martin Schijf <mschijf@gmail.com>
2025-02-23 10:41:02 +0100

    generalized backup scripts

--- backup.sh
+++ backup.sh
@@ -1,21 +1,36 @@
+if [ -z $1 ]
+then
+	postfix=_postgres
+else
+	postfix=$1	
+fi
+
+if [ -z $2 ]
+then
+	keep=672
+else
+	keep=$2
+fi
+
 backuptime=$(date +%Y%m%d_%H%M%S)
 
 backupdir=/tmp/home-monitor-backup 
 mkdir $backupdir
 cd $backupdir
 
-docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
-tar -czf backup_postgres.tar.gz ./backup_postgres.sql
+echo "Starting backup with postfix '$postfix' and keeping last $keep backups"
+
+docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup.sql
+tar -czf backup.tar.gz ./backup.sql
 
 echo -n $(date '+%Y-%m-%d %H:%M:%S')  
 echo -n '  '
-du -h0 backup_postgres.tar.gz
-echo -n '  '
-~/dropbox-uploader/dropbox_uploader.sh upload $backupdir/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
+du backup.tar.gz
+~/dropbox-uploader/dropbox_uploader.sh upload $backupdir/backup.tar.gz Backup/home-monitor/${backuptime}${postfix}
 
 script_path=$(dirname $(realpath -s $0))
-$script_path/cleanup_backup.sh _postgres 672
+$script_path/cleanup_backup.sh $postfix $keep 
 
-rm backup_postgres.tar.gz
-rm backup_postgres.sql
+rm backup.tar.gz
+rm backup.sql
 rmdir $backupdir
