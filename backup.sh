31d3359c7dd20509990db8e7cf228a7333989fa6
Martin Schijf <mschijf@gmail.com>
2025-02-23 16:07:57 +0100

    cleandup backup script: less output

--- backup.sh
+++ backup.sh
@@ -8,18 +8,18 @@ fi
 backuptime=$(date +%Y%m%d_%H%M%S)
 
 backupdir=/tmp/home-monitor-backup 
-mkdir $backupdir
+mkdir -p $backupdir
 cd $backupdir
-
-echo "Starting backup with postfix '$postfix'"
 
 docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup.sql
 tar -czf backup.tar.gz ./backup.sql
 
-echo -n $(date '+%Y-%m-%d %H:%M:%S')  
-echo -n '  '
-du backup.tar.gz
-~/dropbox-uploader/dropbox_uploader.sh upload $backupdir/backup.tar.gz Backup/home-monitor/${backuptime}${postfix}
+dropbox_target=Backup/home-monitor/${backuptime}${postfix}
+~/dropbox-uploader/dropbox_uploader.sh -q upload $backupdir/backup.tar.gz $dropbox_target
+
+echo -n $dropbox_target
+echo -n "  "
+du $backupdir/backup.tar.gz
 
 rm backup.tar.gz
 rm backup.sql
